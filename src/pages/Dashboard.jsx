a4f79a3235ac04b061c8cd8896efdd085489134f
Hasan <hasan.yilbat@gmail.com>
2022-07-31 02:55:03 +0300

    appRouter, updateblog, details

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,19 @@
+import { Box, Container, Grid } from "@mui/material";
 import React from "react";
 import BlogCard from "../components/BlogCard";
 
 const Dashboard = () => {
   return (
-    <div>
+    <Container
+      maxWidth="xl"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
       <BlogCard />
-    </div>
+    </Container>
   );
 };
 
